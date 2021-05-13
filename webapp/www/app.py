67d48a0ddbe2336d7f775b54f976b2899aaf45de
AmamiyaRen <Amamiya_R@outlook.com>
2021-05-13 20:04:41 +0800

    day 2

--- webapp/www/app.py
+++ webapp/www/app.py
@@ -1,28 +1,30 @@
-#!/usr/bin python3
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 __author__ = 'AmamiyaRen'
 
 '''
-async web application.
+导入 logging 模块并使用';'对其全局配置
+logging 模块用法：参考自 https://zhuanlan.zhihu.com/p/56968001
+basicConfig 配置了 level 信息，level 配置为 INFO 信息，即只输出 INFO 级别的信息
+logging 日志的级别可参考下面的官方文档
+https://docs.python.org/zh-cn/3.8/library/logging.html?highlight=logging#logging-levels
 '''
 
-import logging
-
-from aiohttp.web_routedef import head; logging.basicConfig(level=logging.INFO)
-
-import asyncio, os, json, time
-from datetime import datetime
-
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio
 from aiohttp import web
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
+async def index(request):
+    # 请求---》web---》响应
+    # request         response
+    return web.Response(body=b'<h1>Awesome</h1>', headers={'content-type': 'text/html'})
+
 
 async def init(loop):
-    app = web.Application(loop = loop)
-    app.router.add_route('GET','/',index)
-    srv = await loop.create_server(app.make_handler(),'192.168.10.40',9000)
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '192.168.10.40', 9000)
     logging.info('server started at http://192.168.10.40:9000...')
     return srv
 
--- webapp/www/orm.py
+++ webapp/www/orm.py
@@ -1,5 +1,5 @@
-#!/usr/bin python3
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 __author__ = 'AmamiyaRen'
 
@@ -7,3 +7,49 @@ __author__ = 'AmamiyaRen'
 orm
 '''
 
+import asyncio
+import logging
+import aiomysql
+
+
+def log(sql, arfs=()):
+    logging.info('SQL:%s' % sql)
+
+
+async def create_pool(loop, **kw):
+    logging.info('创建数据库链接池...')
+    #用于创建链接池
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),           #默认定义host为localhost
+        port=kw.get('port', 3306),                  #默认定义mysql的端口号为3306
+        user=kw['user'],                            #user是通过关键字参数传递进来
+        password=kw['password'],                    #password是通过关键字参数传递进来
+        db=kw['db'],                                #数据库名字
+        charset=kw.get('charset', 'utf8mb4'),       #默认数据库字符集为utf8mb4
+        autocommit=kw.get('autocommit', True),      #默认自动提交事务
+        maxsize=kw.get('maxsize', 10),              #连接池最多同时处理10个请求
+        minsize=kw.get('minsize', 1),               #链接池最少1个请求
+        loop=loop                                   #传递消息循环对象loop用于异步执行
+    )
+
+
+#用于SQL的select语句。对应select方法，传入sql语句和参数
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    #异步等待连接池对象返回可以连接线程，with语句封装了清理\关闭conn和处理异常
+    async with __pool.get() as conn:
+        #等待连接对象返回DictCursor可以通过dice的方式取得数据库对象，需要通过游标对象执行SQL
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            #将sql中的'?'替换为'%s'，因为mysql语句中的占位符为%s
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            #如果传入size
+            if size:
+                #从数据库获取指定的行数
+                rs = await cur.fetchmany(size)
+            else:
+                #返回所有的结果集
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
